a3355d8b81662d6acdb00fb337d8db9454a0e0bf
Raymond Kwan <kwanray@users.noreply.github.com>
2026-06-07 16:14:09 +0800

    feat: wire handleSearchNavigate as onNavigate into PointView

--- components/RootedApp.tsx
+++ components/RootedApp.tsx
@@ -254,6 +254,7 @@ export default function RootedApp() {
           onBack={handleBack}
           onSearch={handleOpenSearch}
           onHome={() => setState({ ...INITIAL_STATE, screen: 'welcome' })}
+          onNavigate={handleSearchNavigate}
         />
       )}
 
